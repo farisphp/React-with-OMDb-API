27b99dc02b8de8c5afc57824a4cc250c332ec3a7
Faris Perwira Haqi <faris.perwira@gmail.com>
2021-09-21 01:28:26 +0700

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 const apiKey = process.env.REACT_APP_API_KEY || "";
 
 export default axios.create({
-  baseURL: `http://www.omdbapi.com/?apikey=${apiKey}&`,
+  baseURL: `https://www.omdbapi.com/?apikey=${apiKey}&`,
   responseType: "json"
 });
